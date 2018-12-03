da8d6744a82afe496718d1ef72a14c28abe6bb3b
ChanJuSin <ckswn1259@naver.com>
2018-12-03 18:21:13 +0900

    게시글 수정 기능 구현, 비즈니스 계층 구현

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,54 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+/*
+ * 	@AllArgsConstructor
+ * 	 --- 해당 클래스의 인스턴스 변수들을 생성자의 파라미터로 받는다.
+ *   --- 아래 클래스의 경우 BoardServiceImpl 인스턴스를 생성하면서 BoardMapper 인터페이스의 구현체를 주입 받는다.
+ */
+@Log4j
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+	private BoardMapper mapper;
+	
+	@Override
+	public void register(BoardVO board) {
+		log.info("register......" + board);
+		mapper.insertSelectKey(board);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		log.info("get.........." + bno);
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		log.info("modify........" + board);
+		return mapper.update(board) == 1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		log.info("remove........" + bno);
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		log.info("getList...........");
+		return mapper.getList();
+	}
+
+}
--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -38,9 +38,20 @@ public class BoardMapperTests {
 		log.info(board);
 	}
 	
-	@Test
 	public void testDelete() {
 		log.info("DELETE COUNT : " + mapper.delete(1L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT : " + count);
 	}
 	
 }
