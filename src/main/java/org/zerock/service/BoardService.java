7bf1ed21cdd0c10ba4cd4b2508a858c9c59a3731
ChanJuSin <ckswn1259@naver.com>
2018-12-05 15:46:18 +0900

    페이징 처리 기능 추가

--- src/main/java/org/zerock/service/BoardService.java
+++ src/main/java/org/zerock/service/BoardService.java
@@ -3,9 +3,12 @@ package org.zerock.service;
 import java.util.List;
 
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardService {
 
+	public List<BoardVO> getList(Criteria cri);
+	
 	public void register(BoardVO board);
 	
 	public BoardVO get(Long bno);
@@ -13,7 +16,5 @@ public interface BoardService {
 	public boolean modify(BoardVO board);
 	
 	public boolean remove(Long bno);
-	
-	public List<BoardVO> getList();
 	
 }
--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,11 +1,15 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.service.BoardService;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -16,10 +20,14 @@ import lombok.extern.log4j.Log4j;
 public class BoardMapperTests {
 
 	@Setter(onMethod_ = { @Autowired })
+	private BoardService service;
+	
+	@Setter(onMethod_ = { @Autowired })
 	private BoardMapper mapper; 
 	
+	@Test
 	public void testGetList() {
-		mapper.getList().forEach(board -> log.info(board));
+		service.getList(new Criteria(2, 10)).forEach(board -> log.info(board));
 	}
 
 	public void testInsert() {
@@ -41,8 +49,7 @@ public class BoardMapperTests {
 	public void testDelete() {
 		log.info("DELETE COUNT : " + mapper.delete(1L));
 	}
-	
-	@Test
+
 	public void testUpdate() {
 		BoardVO board = new BoardVO();
 		board.setBno(5L);
@@ -52,6 +59,14 @@ public class BoardMapperTests {
 		
 		int count = mapper.update(board);
 		log.info("UPDATE COUNT : " + count);
+	}
+
+	public void testPaging() {
+		Criteria cri = new Criteria();
+		cri.setAmount(10);
+		cri.setPageNum(3);
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board));
 	}
 	
 }
