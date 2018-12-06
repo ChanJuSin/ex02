10f38c461875f9c86f9454b524da780fe3c36125
ChanJuSin <ckswn1259@naver.com>
2018-12-06 17:15:44 +0900

    UriComponentsBuilder 이용 페이징, 검색처리 데이터 유지 기능 추가

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -63,13 +63,14 @@ public class BoardController {
 			rttr.addFlashAttribute("result", "success");
 		}
 		
-		// redirect를 보내면서 추가할 파라메터
+		/*// redirect를 보내면서 추가할 파라메터
 		rttr.addAttribute("pageNum", cri.getPageNum());
 		rttr.addAttribute("amount", cri.getAmount());
 		rttr.addAttribute("type", cri.getType());
-		rttr.addAttribute("keyword", cri.getKeyword());
+		rttr.addAttribute("keyword", cri.getKeyword());*/
 		
-		return "redirect:/board/list";
+		// 페이징 처리, 검색 처리에 필요한 유지되는 데이터를 getListLink 메소드 하나로 처리함
+		return "redirect:/board/list" + cri.getListLink();
 	}
 
 	// Criteria는 페이징 처리후 3페이지에서 게시물을 조회하고 삭제핧경우 삭제된뒤 다시 3페이지를 보여주기 위해 필요하다.
@@ -81,13 +82,13 @@ public class BoardController {
 			rttr.addFlashAttribute("result", "success");
 		}
 
-		// redirect를 보내면서 추가할 파라메터
+		/*// redirect를 보내면서 추가할 파라메터
 		rttr.addAttribute("pageNum", cri.getPageNum());
 		rttr.addAttribute("amount", cri.getAmount());
 		rttr.addAttribute("type", cri.getType());
-		rttr.addAttribute("keyword", cri.getKeyword());
+		rttr.addAttribute("keyword", cri.getKeyword());*/
 		
-		return "redirect:/board/list";
+		return "redirect:/board/list" + cri.getListLink();
 	}
 	
 }
--- src/main/java/org/zerock/domain/Criteria.java
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -1,5 +1,7 @@
 package org.zerock.domain;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -25,6 +27,21 @@ public class Criteria {
 	
 	public String[] getTypeArr() {
 		return type == null ? new String[] {} : type.split("");
+	}
+	
+	/*
+	 *	CLASS UriComponentsBuilder
+	 * 	 --- get 방식에서 유지해야할 정보들을 알아서 ?test="test" 의 형식으로 만들어줌
+	 *   --- 한글처리를 하지 않아도 되는 장점이 있음
+	 */
+	public String getListLink() {
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+			.queryParam("pageNum", this.pageNum)
+			.queryParam("amount", this.getAmount())
+			.queryParam("type", this.getType())
+			.queryParam("keyword", this.getKeyword());
+		
+		return builder.toUriString();
 	}
 	
 }
